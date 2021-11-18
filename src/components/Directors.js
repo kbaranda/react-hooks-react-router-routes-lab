c73d258857a7efe149f103beac6d213177484d55
Kat <bolivarkat@yahoo.com>
2021-11-18 12:52:38 -0600

    Completed assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,25 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director) => {
+        return (
+          <div key={director.name}>
+            <p>{director.name}</p>
+            {director.movies.map((movie) => {
+              return (
+                <ul key={movie}>
+                  <li>{movie}</li>
+                </ul>
+              )
+            })}
+          </div>
+        )
+      })}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,24 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Movies Page</h1>
+      <div>
+        {movies.map((movie) => {
+          return (
+            <div key={movie.title}>
+              <p>{movie.title}</p>
+              <p>{movie.time}</p>
+              <ul>
+                <li>{movie.genres}</li>
+              </ul>
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Movies;
